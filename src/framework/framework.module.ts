5e95bda96357bb1f20aaff3fc40bb7daccbaf751
Drew Young <drewyoung@quickenloans.com>
2017-09-18 14:06:37 -0400

    Update framework with ResponsiveModule
    Added responsive module and utilized it within header generation

--- src/framework/framework.module.ts
+++ src/framework/framework.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ResponsiveConfig, ResponsiveModule } from "ng2-responsive";
 
 import { FrameworkBodyComponent } from "./framework-body/framework-body.component";
 import { ContentComponent } from "./content/content.component";
@@ -7,9 +8,25 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { MenuComponent } from "./menus/menu/menu.component";
 
+let config = {
+    breakPoints: {
+        xs: { max: 600 },
+        sm: { min: 601, max: 959 },
+        md: { min: 960, max: 1279 },
+        lg: { min: 1280, max: 1919 },
+        xl: { min: 1920 }
+    },
+    debounceTime: 100 // allow to debounce checking timer
+};
+
+export function ResponsiveDefinition() {
+    return new ResponsiveConfig(config);
+};
+
 @NgModule({
     imports: [
         CommonModule,
+        ResponsiveModule
     ],
     declarations: [
         FrameworkBodyComponent,
@@ -18,9 +35,16 @@ import { MenuComponent } from "./menus/menu/menu.component";
         FooterComponent,
         MenuComponent
     ],
-    providers: [],
+    providers: [
+        {
+            provide: ResponsiveConfig,
+            useFactory: ResponsiveDefinition
+        }
+    ],
     exports: [
-        FrameworkBodyComponent
+        FrameworkBodyComponent,
+        ResponsiveModule
     ]
 })
+
 export class FrameworkModule { }
