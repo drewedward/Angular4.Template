ee13ac223b2429f6bdcc03fcccc3567bc28934fb
Drew Young <drewyoung@quickenloans.com>
2017-09-19 20:00:36 -0400

    Added new component to resource-one-module

--- src/app/resource-one/resource-one-routing.module.ts
+++ src/app/resource-one/resource-one-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResourceOneComponent } from './resource-one.component';
+import { ResourceOneListComponent } from './components/resource-one-list/resource-one-list.component';
 
 const routes: Routes = [
-    { path: '', component: ResourceOneComponent }
+    { path: '', component: ResourceOneComponent },
+    { path: 'resource-one-list', component: ResourceOneListComponent }
 ];
 
 @NgModule({
--- src/app/resource-one/resource-one.module.ts
+++ src/app/resource-one/resource-one.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ResponsiveModule } from 'ng2-responsive';
 import { ResourceOneRoutingModule } from './resource-one-routing.module';
 import { ResourceOneComponent } from './resource-one.component';
+import { ResourceOneListComponent } from './components/resource-one-list/resource-one-list.component';
 
 @NgModule({
     imports: [
@@ -11,7 +12,8 @@ import { ResourceOneComponent } from './resource-one.component';
         ResourceOneRoutingModule
     ],
     declarations: [
-        ResourceOneComponent
+        ResourceOneComponent,
+        ResourceOneListComponent
     ]
 })
 export class ResourceOneModule { }
