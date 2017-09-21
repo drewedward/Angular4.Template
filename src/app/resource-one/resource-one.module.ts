98c48a80b03fe51864a69a0c017292709a41fddb
Drew Young <drewyoung@quickenloans.com>
2017-09-21 15:42:36 -0400

    Added create resource button

--- src/app/resource-one/resource-one.module.ts
+++ src/app/resource-one/resource-one.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResponsiveModule } from 'ng2-responsive';
+import { ButtonModule } from 'primeng/primeng';
 import { ResourceOneRoutingModule } from './resource-one-routing.module';
+
 import { ResourceOneComponent } from './resource-one.component';
 import { ResourceOneListComponent } from './components/resource-one-list/resource-one-list.component';
 import { ResourceOneDetailsComponent } from './components/resource-one-details/resource-one-details.component';
+
 
 @NgModule({
     imports: [
         CommonModule,
         ResponsiveModule,
+        ButtonModule,
         ResourceOneRoutingModule
     ],
     declarations: [
